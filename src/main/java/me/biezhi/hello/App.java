ad82a19b44265f5d20e08f6d80007b53665d6282
biezhi <biezhi.me@gmail.com>
2015-10-10 16:23:27 +0800

    -

---
+++ src/main/java/me/biezhi/hello/App.java
@@ -0,0 +1,46 @@
+package me.biezhi.hello;
+
+import com.blade.Blade;
+import com.blade.Bootstrap;
+import com.blade.route.RouteHandler;
+import com.blade.route.RouterExecutor;
+import com.blade.servlet.Request;
+import com.blade.servlet.Response;
+
+import blade.kit.json.JSONObject;
+
+/**
+ * Hello Blade!
+ *
+ */
+public class App extends Bootstrap {
+
+	@Override
+	public void init() {
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Blade blade = Blade.me();
+		/**
+		 * JDK1.6
+		 */
+		blade.get("/", new RouteHandler() {
+			public Object handler(Request request, Response response) {
+				response.html("<h1>Hello Blade！</h1>");
+				return null;
+			}
+		});
+		
+		// java8
+		blade.get("/hello").run( (req,res) -> {
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("name", "biezhi");
+			jsonObject.put("blog", "https://biezhi.me");
+			res.json(jsonObject.toString());
+			return null;
+		});
+		
+		blade.app(App.class).listen(9001).start();
+	}
+	
+}
